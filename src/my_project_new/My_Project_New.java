d041d8aa27dadb8b279653734243ed9bdec00509
Asad Muneer <sheikhasadmuneer@hotmail.com>
2016-11-18 00:54:00 +0500

    source files

---
+++ src/my_project_new/My_Project_New.java
@@ -0,0 +1,20 @@
+    
+ package my_project_new;
+import All_Gui.NotePad;
+/**
+ *
+ * @author Sheikh Asad
+ */
+public class My_Project_New {
+ 
+    public static void main(String[] args) {
+        
+        NotePad obj = new NotePad(); // makes object of NotePad Class //
+        obj.setVisible(true);        // makes form visible //
+        obj.setLocationRelativeTo(null); // sets application to the middle of screen //
+        
+        
+        
+        
+    }
+}
